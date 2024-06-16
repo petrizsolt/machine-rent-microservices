6741151932ea27a43878561da50c8a7df40e7688
Petri Zsolt <petrizsolt@outlook.com>
2024-06-16 12:02:32 +0200

    Rental entity

---
+++ rental-microservice/src/main/java/hu/machinerental/rental_microservice/model/entity/Rental.java
@@ -0,0 +1,34 @@
+package hu.machinerental.rental_microservice.model.entity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "rentals")
+public class Rental {
+
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.UUID)
+	  private UUID id;
+	  
+	  private UUID machineId;
+	  
+	  private UUID customerId;
+	  
+	  private LocalDate dateFrom;
+	  
+	  private LocalDate dateTo;
+
+}
